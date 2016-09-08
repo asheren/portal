7f7f2d14006d7ca1728ac255ec02f827a72b8a81
Allison McMillan <allison.p.mcmillan@gmail.com>
2016-09-08 16:34:41 -0400

    test commit

--- lib/portal.ex
+++ lib/portal.ex
@@ -17,6 +17,7 @@ defmodule Portal do
     opts = [strategy: :simple_one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
   end
+  #test commit
 
   @doc """
   Shoots a new door with the given `color.
